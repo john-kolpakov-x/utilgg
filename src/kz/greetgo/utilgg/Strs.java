b9fddff3412fc52942dd0904755ac393b42e7b71
pompei <john.kolpakov.x@gmail.com>
2015-12-03 22:03:37 +0600

    Killed left finals

--- src/kz/greetgo/utilgg/Strs.java
+++ src/kz/greetgo/utilgg/Strs.java
@@ -4,17 +4,17 @@ public class Strs {
   private Strs() {
   }
 
-  public static final String upper(String str) {
+  public static String upper(String str) {
     if (str == null) return null;
     return str.toUpperCase();
   }
 
-  public static final String lower(String str) {
+  public static String lower(String str) {
     if (str == null) return null;
     return str.toLowerCase();
   }
 
-  public static final String firstLower(String str) {
+  public static String firstLower(String str) {
     if (str == null) return null;
     int length = str.length();
     if (length == 0) return "";
@@ -22,7 +22,7 @@ public class Strs {
     return str.substring(0, 1).toLowerCase() + str.substring(1);
   }
 
-  public static final String firstUpper(String str) {
+  public static String firstUpper(String str) {
     if (str == null) return null;
     int length = str.length();
     if (length == 0) return "";
